4741f66db724920d3509dd75aef768f66ce05b57
femga <47989639+femga@users.noreply.github.com>
2020-10-18 02:51:42 +0300

    Created frontend soundsets list.

---
+++ audio/frontend_soundsets/README.md
@@ -0,0 +1,33 @@
+## FRONTEND SOUNDSETS
+
+Some frontend_soundset_refs need according audio banks loaded! Usually name of audio bank is the name of frontend_soundset_ref without '\_Sounds' or '\_soundset'. You can see audio banks here: https://github.com/femga/rdr3_discoveries/blob/master/audio/audio_banks/audio_banks.lua
+
+## Example of playing frontend soundsets:
+
+```
+
+local is_frontend_sound_playing = false
+local frontend_soundset_ref = "Player_Core_Empty_Sounds"
+local frontend_soundset_name =  "DEADEYE"
+
+Citizen.CreateThread(function()
+  while true do
+      Citizen.Wait(0) 
+      if Citizen.InvokeNative(0x91AEF906BCA88877,0, 0x17BEC168) then   -- pressed E
+        if not is_frontend_sound_playing then           
+          if frontend_soundset_ref ~= 0 then
+            Citizen.InvokeNative(0x0F2A2175734926D8,frontend_soundset_name, frontend_soundset_ref);   -- load sound frontend
+          end    
+          Citizen.InvokeNative(0x67C540AA08E4A6F5,frontend_soundset_name, frontend_soundset_ref, true, 0);  -- play sound frontend
+          is_frontend_sound_playing = true
+          print("sound frontend is playing")
+        else
+          Citizen.InvokeNative(0x9D746964E0CF2C5F,frontend_soundset_name, frontend_soundset_ref)  -- stop audio
+          is_frontend_sound_playing = false
+          print("sound frontend is stopped")
+        end
+      end
+  end
+end)
+
+```
---
+++ audio/frontend_soundsets/frontend_soundsets.lua
@@ -0,0 +1,334 @@
+-- [frontend_soundset_ref] = frontend_soundset_name,
+
+local frontend_soundsets = {
+
+    ["DEATH_FAIL_RESPAWN_SOUNDS"] = {
+      "DEATH_SCREEN_ENTER",
+      "DEATH_SCREEN_EXIT",
+      "EXIT",
+      "FADE_TO_BLACK",
+      "FADE_TO_BLACK_COMPANIONACTIVITY",
+      "FADE_TO_BLACK_LOANSHARK",
+      "FADE_TO_BLACK_OFF_MISSION",
+      "HORSE_FALL_FAIL",
+      "TITLE_SCREEN_ENTER",
+      "TITLE_SCREEN_EXIT",
+      "TITLE_SCREEN_EXIT_OFF_MISSION",
+    },
+
+    ["PAUSE_MENU_SOUNDSET"] = {
+      "NAV_LEFT",
+      "NAV_RIGHT",
+    },
+
+
+    ["Mission_Complete_Sounds"] = {
+      "Mission_Complete_Final_Wipe_Off",
+    },
+
+    ["Ledger_Sounds"] = {
+      "INFO_HIDE",
+      "INFO_SHOW",
+      "NAV_DOWN",
+      "NAV_LEFT",
+      "NAV_RIGHT",
+      "NAV_UP",
+      "PURCHASE",
+      "UNAFFORDABLE",
+      "upgrade_cam_pan",
+    },
+
+    ["Player_Core_Empty_Sounds"] = {
+      "DEADEYE",
+      "HEALTH",
+      "HEALTH_VFX_FLASH",
+      "STAMINA",
+    },
+
+    ["HUD_Wanted_Sounds"] = {
+      "Wanted_Spotted",
+      "Witness",
+      "Witness_Wanted",
+    },
+
+    ["MP_Leaderboard_Sounds"] = {
+      "Leaderboard_Hide",
+      "Leaderboard_Show",
+    },
+
+    ["RDRO_In_Game_Menu_Sounds"] = {
+      "Ready_Up_Flash",
+    },
+
+    ["HUD_SHOP_SOUNDSET"] = {
+      "BACK",
+      "INFO",
+      "INFO_HIDE",
+      "PURCHASE",
+      "QUIT",
+      "READ",
+      "SELECT",
+      "SELL",
+    },
+
+    ["HUD_POKER"] = {
+      "BET_AMOUNT",
+      "BET_MIN_MAX",
+      "BET_PROMPT",
+    },
+
+    ["HUD_MINI_GAME_SOUNDSET"] = {
+      "CHECKPOINT_NORMAL",
+      "CHECKPOINT_PERFECT",
+    },
+
+    ["wnt2_CME_SoundSet"] = {
+      "LeavingColter6_Gust",
+    },
+
+    ["HUD_MP_PITP"] = {
+      "Deny_Point",
+      "Gain_Point",
+    },
+
+    ["FMA_ARCHERY_Sounds"] = {
+      "BULLSEYE",
+      "INNER_RING",
+      "OUTER_RING",
+    },
+
+    ["RDRO_Poker_Sounds"] = {
+      "menu_select_bet",
+      "menu_select_default",
+      "menu_select_fold",
+      "player_turn_countdown_end",
+      "player_turn_countdown_start",
+      "round_start_countdown_tick",
+      "showdown_lose",
+      "showdown_win",
+    },
+
+    ["Flex_Fight_Sounds"] = {
+      "Horn",
+      "Whistle",
+    },
+
+    ["RDRO_Countdown_Sounds"] = {
+      "Match_End_Timer",
+    },
+
+    ["RDRO_Holiday_Present_Sounds"] = {
+      "holiday_present_message",
+    },
+
+    ["HUD_MAIL_SOUNDSET"] = {
+      "MAIL",
+    },
+
+    ["HUD_MP_FREE_ROAM"] = {
+      "PURCHASE",
+    },
+
+    ["RDR_Shard_Central_Message_Sounds"] = {
+      "sudden_death",
+    },
+
+    ["Tithe_Donation_Sounds"] = {
+      "Tithe_Donation_Large",
+      "Tithe_Donation_Medium",
+      "Tithe_Donation_Small",
+    },
+
+    ["Study_Sounds"] = {
+      "hide_info",
+      "show_info",
+    },
+
+    ["Consumption_Sounds"] = {
+      "Core_Fill_Up",
+      "Core_Full",
+    },
+
+    ["RDRO_Spectate_Sounds"] = {
+      "hide_hud",
+      "left_bumper",
+      "right_bumper",
+    },
+
+    ["MP005_OADPRH_sounds"] = {
+      "match_lost",
+      "match_won",
+    },
+
+    ["SAL1_Sounds"] = {
+      "JumpCut",
+    },
+
+    ["HUD_MP_FREE_MODE"] = {
+      "EVENT_AVAILABLE",
+      "HP_HORSE",
+    },
+
+    ["Objective_Sounds"] = {
+      "FAIL",
+    },
+
+    ["gng3_CME_SoundSet"] = {
+      "prison_shot",
+      "watchtower_shot",
+    },
+
+    ["RDROADV_Winners_Screen_Sounds"] = {
+      "Winners_Screen",
+    },
+
+    ["RDRO_Winners_Screen_Sounds"] = {
+      "dislike",
+      "enter",
+      "like",
+    },
+
+    ["SSCRFT_Sounds"] = {
+      "Craft_Items_Enter",
+      "Exit",
+    },
+
+    ["RDRO_Race_sounds"] = {
+      "321_GO",
+    },
+
+    ["RDRO_Out_Of_Bounds_Sounds"] = {
+      "OOB_death",
+      "OOB_return",
+      "OOB_start",
+    },
+
+    ["HUD_REWARD_SOUNDSET"] = {
+      "ITEM_TICKER_GENERIC",
+      "REWARD_MORALE",
+      "REWARD_NEW_GUN",
+    },
+
+    ["RDRO_Adversary_Sounds"] = {
+      "match_start",
+    },
+
+    ["RDRO_King_Of_The_Rails_Sounds"] = {
+      "new_active_carriage",
+      "screen_pulse",
+    },
+
+    ["TOWN_SECRET_SOUNDS"] = {
+      "SECRET_FOUND",
+    },
+
+    ["rdro_gamemode_transition_sounds"] = {
+      "exit_photo_to_match",
+      "flash_to_landscape",
+      "flash_to_photo",
+      "photograph",
+    },
+
+    ["RDRO_Collectible_Sounds_Travelling_Saleswoman"] = {
+      "collectible_found",
+    },
+
+    ["Photo_Mode_Sounds"] = {
+      "back",
+      "effects",
+      "filter_left",
+      "filter_right",
+      "hide_hud",
+      "lens_down",
+      "reset",
+      "take_photo",
+      "lens_down",
+      "lens_up",
+    },
+
+    ["HUD_DUEL_SOUNDSET"] = {
+      "HUD_DRAW",
+    },
+
+    ["HUD_Camp_Status_Sounds"] = {
+      "Back",
+    },
+
+    ["HUD_Donate_Sounds"] = {
+      "Amount_Decrease",
+      "Amount_Increase",
+      "Donate",
+      "Exit",
+      "Give_Item_Enter",
+      "Give_Money_Enter",
+      "Give_To_Gang",
+    },
+
+    ["rdch3_CME_SoundSet"] = {
+      "river_fast",
+      "river_med",
+    },
+
+    ["RDRO_Notoriety_Sounds"] = {
+      "Defense_Disabled",
+      "Defense_Enabled",
+    },
+
+    ["RDRO_ADV_Afghan_Polo_Sounds"] = {
+      "new_bout",
+      "pickup_weapon",
+    },
+
+    ["HUD_DOMINOS_SOUNDSET"] = {
+      "MONEY",
+      "NAV_DOWN",
+      "NAV_LEFT",
+      "NAV_RIGHT",
+      "NAV_UP",
+      "NO",
+      "YES",
+    },
+
+    ["MPIAP_Sounds"] = {
+      "match_lost",
+      "match_won",
+    },
+
+    ["RDRO_Character_Creator_Sounds"] = {
+      "BACK",
+      "Camera_Flash",
+      "gender_left",
+      "gender_right",
+      "SELECT",
+    },
+
+    ["RDRO_Countdown_Clockwork_Sounds"] = {
+      "Strike_Heavy",
+      "Strike_Light",
+    },
+
+    ["RDRO_Sniper_Tension_Sounds"] = {
+      "Heartbeat",
+      "Tension_Loop",
+    },
+
+    ["RDRO_ATL_Sounds"] = {
+      "Player_Enter_Line",
+      "Player_Exit_Line",
+    },
+
+    ["RDRO_Idle_Kick_Sounds"] = {
+      "Idle_Kick_Message",
+      "Idle_Kick_Timer_10s",
+    },
+
+    ["RDRO_Camera_Flash_Pulse"] = {
+      "camera_flash",
+    },
+
+    [0] = {
+      "BATHING_DOOR_KNOCK_MASTER",
+      "Friend_Pick_Up",
+    },
+
+}
