dac5592fbe8911f0fe1d4144c00e71ee8057f95f
femga <47989639+femga@users.noreply.github.com>
2021-10-16 11:30:33 +0300

    Added ped outfits

--- peds_customization/ped_outfits.lua
+++ peds_customization/ped_outfits.lua
@@ -24,40 +24,40 @@ local ped_non_requested_outfits = {
 	0x61474899,	-- reinforced_bindings_hands_bulky
 	0xD1C78E0F,	-- reinforced_bindings_feet_default
 	0x6ADC1082,	-- reinforced_bindings_feet_bulky
-	0x8E6F504E, -- player_zero_ammo_pistol_000
-	0x10D1D505, -- player_zero_ammo_pistol_001
-	0xEB8A8A77, -- player_zero_ammo_pistol_002
-	0xFDB92ED4, -- player_zero_ammo_pistol_003
-	0xC828C3C0, -- player_zero_ammo_pistol_004
-	0x2A030773, -- player_zero_ammo_pistol_005
-	0x44B83CDD, -- player_zero_ammo_pistol_006
-	0x09DEC3C9, -- player_zero_ammo_rifle_000
-	0xF7B69F79, -- player_zero_ammo_rifle_001
-	0xE481F90C, -- player_zero_ammo_rifle_002
-	0xD346D696, -- player_zero_ammo_rifle_003
-	0xE007701B, -- player_zero_ammo_rifle_004
-	0xCEED4DE7, -- player_zero_ammo_rifle_005
-	0x934E1605, -- player_zero_beltchain_000
-	0xF52E779C, -- player_zero_ammo_shotgun_000
-	0x569A3A6E, -- player_zero_ammo_shotgun_001
-	0x44E41702, -- player_zero_ammo_shotgun_002
-	0x731BF371, -- player_zero_ammo_shotgun_003
-	0x8F652C03, -- player_zero_ammo_shotgun_004
-	0x0E20A978, -- player_zero_ammo_shotgun_005
-	0x334DAFB2, -- player_zero_mask_002
-	0x346F63AA, -- player_zero_mask_002
-	0x9C41BF79, -- player_zero_bandolier_000
+	0x8E6F504E, 	-- player_zero_ammo_pistol_000
+	0x10D1D505, 	-- player_zero_ammo_pistol_001
+	0xEB8A8A77, 	-- player_zero_ammo_pistol_002
+	0xFDB92ED4, 	-- player_zero_ammo_pistol_003
+	0xC828C3C0, 	-- player_zero_ammo_pistol_004
+	0x2A030773, 	-- player_zero_ammo_pistol_005
+	0x44B83CDD, 	-- player_zero_ammo_pistol_006
+	0x09DEC3C9, 	-- player_zero_ammo_rifle_000
+	0xF7B69F79, 	-- player_zero_ammo_rifle_001
+	0xE481F90C, 	-- player_zero_ammo_rifle_002
+	0xD346D696, 	-- player_zero_ammo_rifle_003
+	0xE007701B, 	-- player_zero_ammo_rifle_004
+	0xCEED4DE7, 	-- player_zero_ammo_rifle_005
+	0x934E1605, 	-- player_zero_beltchain_000
+	0xF52E779C, 	-- player_zero_ammo_shotgun_000
+	0x569A3A6E, 	-- player_zero_ammo_shotgun_001
+	0x44E41702, 	-- player_zero_ammo_shotgun_002
+	0x731BF371, 	-- player_zero_ammo_shotgun_003
+	0x8F652C03, 	-- player_zero_ammo_shotgun_004
+	0x0E20A978, 	-- player_zero_ammo_shotgun_005
+	0x334DAFB2, 	-- player_zero_mask_002
+	0x346F63AA, 	-- player_zero_mask_002
+	0x9C41BF79, 	-- player_zero_bandolier_000
 	0x53436C97,
-	0x897D096D, -- player_zero_cardholdout_000
-	0x9F204EAA, -- cs_moneybag_ms1_01
-	0x1D4FBB2D, -- player_zero_badge_000
+	0x897D096D, 	-- player_zero_cardholdout_000
+	0x9F204EAA, 	-- cs_moneybag_ms1_01
+	0x1D4FBB2D, 	-- player_zero_badge_000
 	0xB5F15852,
-	0x227B481C, -- player_zero_gloves_000_full
+	0x227B481C, 	-- player_zero_gloves_000_full
 	0xA7404E48,	-- player_zero_holster_belt_001
 	0xC21D2125,
-	0x2514B2B9, -- player_zero_hat_000
+	0x2514B2B9, 	-- player_zero_hat_000
 	0x05A94693,	-- player_zero_hat_024
-	0xB2A7CB98, -- player_zero_hat_017
+	0xB2A7CB98, 	-- player_zero_hat_017
 	0xA783B82E,	-- player_zero_hat_026
 	0x2968E73D,	-- player_zero_hat_037
 	0xAE8ACE4E,	-- player_zero_hat_019
@@ -76,13 +76,13 @@ local ped_non_requested_outfits = {
 	0xB245B8FB,	-- player_zero_mask_006
 	0xC60AE41A,	-- player_zero_mask_001
 	0x7D70EEA3,	-- player_zero_mask_001
-	0xBE61FA14, -- player_zero_mask_002
+	0xBE61FA14, 	-- player_zero_mask_002
 	0x6E961736,	-- player_zero_mask_002_winter
 	0x37EB21AA,
 	0xF02B2C96,	-- meta_wearable_money_bag
 	0xE6E93016,	-- meta_wearable_component_mp_moneybags_male
 	0x327A1C4C,	-- player_zero_quiver_000
-	0x7B842B45, -- player_zero_holster_belt_000 + player_zero_holster_cup_000
+	0x7B842B45, 	-- player_zero_holster_belt_000 + player_zero_holster_cup_000
 	0xFAB65926,	-- player_zero_satchel_000
 	0x821DB210,	-- player_zero_satchel_strap_000
 	0x2F8CE311,	-- player_zero_satchel_strap_000_lw
@@ -99,7 +99,7 @@ local ped_non_requested_outfits = {
 	0x96B481A2,	-- player_zero_tal_buffalo_horn_000
 	0xF49206A6,	-- player_zero_tal_eagle_talon_000
 	0x37248913,	-- player_zero_tal_raven_claw_000
-	0x29932164, -- meta_wearable_component_mp_moneybags_female
+	0x29932164, 	-- meta_wearable_component_mp_moneybags_female
 
 	0x86155956,	-- waist size + stomach size (for mp_male and mp_female) -- smallest
 	0x97F0FD0D,	-- waist size + stomach size (for mp_male and mp_female)
@@ -135,17 +135,17 @@ local ped_non_requested_outfits = {
 	0x33DDD0CB,	-- upperbody size	
 	0x1BC3A19B,	-- upperbody size	 -- biggest
 
-	0xBB7091D9, -- waist size (for player_zero)	-- biggest
-	0xCF85BA03, -- waist size (for player_zero)	
-	0x17BECA74, -- waist size (for player_zero)	
-	0x29EA6ECB, -- waist size (for player_zero)	
-	0xF8DB8CAE, -- waist size (for player_zero)	
-	0x5AFE50F2, -- waist size (for player_zero)	
-	0x6EFC78EE, -- waist size (for player_zero)	
-	0x42499F89, -- waist size (for player_zero)	
-	0x4420A337, -- waist size (for player_zero)	
-	0x95C7C684, -- waist size (for player_zero)	
-	0x652668B6, -- waist size (for player_zero)	-- smallest
+	0xBB7091D9, 	-- waist size (for player_zero)	-- biggest
+	0xCF85BA03, 	-- waist size (for player_zero)	
+	0x17BECA74, 	-- waist size (for player_zero)	
+	0x29EA6ECB, 	-- waist size (for player_zero)	
+	0xF8DB8CAE, 	-- waist size (for player_zero)	
+	0x5AFE50F2, 	-- waist size (for player_zero)	
+	0x6EFC78EE, 	-- waist size (for player_zero)	
+	0x42499F89, 	-- waist size (for player_zero)	
+	0x4420A337, 	-- waist size (for player_zero)	
+	0x95C7C684, 	-- waist size (for player_zero)	
+	0x652668B6, 	-- waist size (for player_zero)	-- smallest
 
 
 
