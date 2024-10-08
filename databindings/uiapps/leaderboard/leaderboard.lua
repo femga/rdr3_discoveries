e4948e55d55079e4d596e71287c143dea25e6a3e
Valentin CHENE <66779630+Sarbatore@users.noreply.github.com>
2024-10-08 15:19:29 +0200

    leaderboard.lua done

--- databindings/uiapps/leaderboard/leaderboard.lua
+++ databindings/uiapps/leaderboard/leaderboard.lua
@@ -5,7 +5,16 @@ function ShowLeaderboard(headingTable, rowsTable)
     local titleContainer = DatabindingAddDataContainer(rootContainer, "Title")
     local leaderboardList = DatabindingAddUiItemList(rootContainer, "LeaderboardList")
     DatabindingClearBindingArray(leaderboardList)
-
+    
+    DatabindingAddDataBool(leaderboardList, "info_visible_06", true)
+    DatabindingAddDataBool(leaderboardList, "info_visible_07", false)
+    DatabindingAddDataBool(leaderboardList, "info_visible_08", false)
+    DatabindingAddDataString(leaderboardList, "info_value_06", "info_value_06")
+    DatabindingAddDataString(leaderboardList, "info_value_07", "info_value_07")
+    DatabindingAddDataString(leaderboardList, "info_value_08", "info_value_08")
+    DatabindingAddDataString(leaderboardList, "AvatarDictionary", "mp_lobby_textures")
+    DatabindingAddDataString(leaderboardList, "AvatarTexture", "TEMP_PEDSHOT")
+    
     DatabindingAddDataString(titleContainer, "Heading", headingTable.text)
     DatabindingAddDataInt(titleContainer, "HeadingColor", headingTable.color)
 
@@ -17,7 +26,30 @@ function ShowLeaderboard(headingTable, rowsTable)
     DatabindingAddDataInt(titleContainer, "StatRoundsColor", headingTable.round.color)
 
     for i, row in pairs(rowsTable) do
-        local rowContainer = DatabindingAddDataContainer(leaderboardList, -1, "LeaderboardListItem")
+        local rowContainer = DatabindingAddDataContainer(leaderboardList, "LeaderboardListItem_" .. i)
+        DatabindingAddDataInt(rowContainer, "Position", 0)
+        DatabindingAddDataBool(rowContainer, "ShowCross", row.showCross)
+        DatabindingAddDataHash(rowContainer, "CrossColor", row.crossColor)
+        DatabindingAddDataString(rowContainer, "Gamertag", row.gamertag)
+        DatabindingAddDataInt(rowContainer, "GamertagColor", row.gamertagColor)
+        DatabindingAddDataBool(rowContainer, "ShowCrewTag", row.crewTag ~= nil)
+        DatabindingAddDataString(rowContainer, "CrewTag", row.crewTag)
+        DatabindingAddDataBool(rowContainer, "ShowRank", row.rank ~= nil)
+        DatabindingAddDataString(rowContainer, "Rank", row.rank)
+        DatabindingAddDataBool(rowContainer, "Spectating", row.spectating)
+        for j, stat in pairs(row.stats) do
+            DatabindingAddDataString(rowContainer, "Stat" .. j-1, stat)
+        end
+        DatabindingAddDataBool(rowContainer, "ShowBlip", row.blip ~= nil)
+        DatabindingAddDataString(rowContainer, "Blip", row.blip)
+        DatabindingAddDataInt(rowContainer, "BlipColor", row.blipColor)
+        DatabindingAddDataBool(rowContainer, "isHighlighted", row.isHighlighted)
+        DatabindingAddDataBool(rowContainer, "HeadsetIconVisible", row.headsetIconVisible)
+        DatabindingAddDataHash(rowContainer, "HeadsetIconColor", row.headsetIconColor)
+        DatabindingAddDataInt(rowContainer, "SetOverlayImg", 0)
+        DatabindingAddDataBool(rowContainer, "ShowOverlay", row.showOverlay)
+ 
+        DatabindingInsertUiItemToListFromContextStringAlias(leaderboardList, -1, "rowContainer", rowContainer)
     end
 end
 
@@ -59,7 +91,25 @@ local headingTable = {
 
 local rowsTable = {
     {
-
+        showCross = true,
+        crossColor = `COLOR_RED`,
+        gamertag = "Player 1",
+        gamertagColor = `COLOR_RED`,
+        crewTag = "Crew 1",
+        rank = "1",
+        spectating = false,
+        stats = {
+            "10",
+            "5",
+            "3",
+            "8",
+        },
+        blipColor = `COLOR_RED`,
+        blip = "BLIP_AMBIENT_PED_MEDIUM",
+        isHighlighted = false,
+        headsetIconVisible = false,
+        headsetIconColor = `COLOR_PURE_WHITE`,
+        showOverlay = false,
     },
 }
 
